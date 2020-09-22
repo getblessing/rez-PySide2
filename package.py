b30cb6e2fb111a54beb748a22c34fa81980612de
David Lai <davidlatwe@gmail.com>
2020-09-22 22:53:47 +0800

    Adopt pipz --bundle

--- package.py
+++ package.py
@@ -4,20 +4,22 @@ name = "PySide2"
 description = "Python bindings for the Qt cross-platform application " \
               "and UI framework"
 
-version = "5.15.0"
+version = "5.15"
 
-requires = [
-    "shiboken2==5.15.0"
-]
+requires = []
 
 variants = [
     ["os-*", "python-3.6"],
     ["os-*", "python-3.7"],
 ]
 
+pip_packages = [
+    "PySide2==5.15",
+    "shiboken2==5.15",
+]
 
-private_build_requires = ["rezutil-1", "pipz"]
-build_command = "python -m rezutil build {root} --use-pipz"
+private_build_requires = ["pipz"]
+build_command = "install %s --bundle" % " ".join(pip_packages)
 
 
 def commands():
